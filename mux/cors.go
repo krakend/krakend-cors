4252bf5d70da75abb36b396394706f29d19b0a4e
taik0 <suikogaiden@gmail.com>
2018-06-05 01:07:23 +0200

    Fix New constructor.

--- mux/cors.go
+++ mux/cors.go
@@ -10,20 +10,18 @@ import (
 
 // New returns a http.Handler with the defined CORS options in the ExtraConfig.
 func New(e config.ExtraConfig) http.Handler {
-	var cfg *krakendcors.Config
-	if tmp, ok := krakendcors.ConfigGetter(e).(*krakendcors.Config); ok {
-		cfg = tmp
+	tmp := krakendcors.ConfigGetter(e)
+	if tmp != nil {
+		if cfg, ok := tmp.(krakendcors.Config); ok {
+			return cors.New(cors.Options{
+				AllowedOrigins:   cfg.AllowOrigins,
+				AllowedMethods:   cfg.AllowMethods,
+				AllowedHeaders:   cfg.AllowHeaders,
+				ExposedHeaders:   cfg.ExposeHeaders,
+				AllowCredentials: cfg.AllowCredentials,
+				MaxAge:           int(cfg.MaxAge.Seconds()),
+			}).Handler(http.NewServeMux())
+		}
 	}
-
-	if cfg == nil {
-		return nil
-	}
-	return cors.New(cors.Options{
-		AllowedOrigins:   cfg.AllowOrigins,
-		AllowedMethods:   cfg.AllowMethods,
-		AllowedHeaders:   cfg.AllowHeaders,
-		ExposedHeaders:   cfg.ExposeHeaders,
-		AllowCredentials: cfg.AllowCredentials,
-		MaxAge:           int(cfg.MaxAge.Seconds()),
-	}).Handler(http.NewServeMux())
+	return nil
 }
